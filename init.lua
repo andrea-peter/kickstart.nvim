aa476296bc2a9e289ba63901ea56883110edaab0
Andrea Peter <andrea.peter@ecorobotix.com>
2026-04-02 10:07:08 +0200

    Add keymap for telescope's oldfiles

--- init.lua
+++ init.lua
@@ -405,6 +405,7 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>r', builtin.oldfiles, { desc = '[R]ecent files' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
