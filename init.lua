241a8fb70a3734611ba538706277e59b4cb8d800
Andrea Peter <pedrudehuere@hotmail.com>
2026-04-12 11:43:14 +0200

    Allow to toggle autoformat, global and buffer
    Also removed user commands to do so since now obsolete

--- init.lua
+++ init.lua
@@ -734,7 +734,7 @@ require('lazy').setup({
         local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
-        -- See user commands FormatEnable and FormatDisable
+        -- Don't format if disabled globally or in buffer
         elseif vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         else
@@ -1000,28 +1000,6 @@ require('lazy').setup({
       lazy = '💤 ',
     },
   },
-})
-
-vim.api.nvim_create_user_command('FormatDisable', function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-    print 'Disabled autoformat for buffer'
-  else
-    vim.g.disable_autoformat = true
-    print 'Disabled autoformat globally'
-  end
-end, {
-  desc = 'Disable autoformat-on-save',
-  bang = true,
-})
-
-vim.api.nvim_create_user_command('FormatEnable', function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-  print 'Enabled autoformat globally'
-end, {
-  desc = 'Re-enable autoformat-on-save',
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
     opts = function(_, opts)
       table.insert(opts['spec'], {
         { '<leader>u', group = '[U]ser interface', mode = { 'n' } },
+        { '<leader>uf', group = 'Auto[f]ormat', mode = { 'n' } },
       })
     end,
   },
@@ -27,6 +28,7 @@ return {
     scope = { enabled = false },
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
+    toggle = { enable = true },
     words = { enabled = false },
   },
   keys = {
@@ -37,10 +39,27 @@ return {
     },
   },
   init = function()
+    -- Getters and setters for autoformat, global and buffer
+    local set_global_autoformat = function(enable) vim.g.disable_autoformat = not enable end
+    local get_global_autoformat = function() return not vim.g.disable_autoformat end
+    local set_buffer_autoformat = function(enable) vim.b.disable_autoformat = not enable end
+    local get_buffer_autoformat = function() return not vim.b.disable_autoformat end
+    local wk_desc_no_text = { enabled = '', disabled = '' }
+
     vim.api.nvim_create_autocmd('User', {
       pattern = 'VeryLazy',
       callback = function()
         Snacks.toggle.diagnostics():map '<leader>ud'
+        Snacks.toggle(
+          'autoformat_global',
+          { name = 'Autoformat - global', get = get_global_autoformat, set = set_global_autoformat, wk_desc = wk_desc_no_text }
+        )
+          :map '<leader>ufg'
+        Snacks.toggle(
+          'autoformat_buffer',
+          { name = 'Autoformat - buffer', get = get_buffer_autoformat, set = set_buffer_autoformat, wk_desc = wk_desc_no_text }
+        )
+          :map '<leader>ufb'
         Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
         Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
