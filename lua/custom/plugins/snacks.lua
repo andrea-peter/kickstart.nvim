6221e1c4babb93b416582893c6ac0d69856d1639
Andrea Peter <andrea.peter@ecorobotix.com>
2026-05-04 07:43:01 +0200

    Add read-only toggle

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -61,6 +61,7 @@ return {
         )
           :map '<leader>ufb'
         Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        Snacks.toggle.option('readonly', { name = 'Readonly' }):map '<leader>ur'
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
         Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
       end,
