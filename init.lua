3b5c0b31286d1c9598df99562e382bcccfc7dccf
Andrea Peter <andrea.peter@ecorobotix.com>
2026-06-03 07:19:41 +0200

    Conform: Add clang-format as .c files formatter

--- init.lua
+++ init.lua
@@ -745,6 +745,7 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
+        c = { 'clang-format' },
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
