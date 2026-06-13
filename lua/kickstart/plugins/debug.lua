84e32a311ad675d5063d88f767f2e2e9c4d59392
Andrea Peter <andrea.peter@ecorobotix.com>
2026-06-13 12:56:51 +0200

    DAP: Add 'all code' and 'my code' Py attach confs

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -76,10 +76,25 @@ return {
       end
     end
 
+    -- Attach, only my code
     table.insert(dap.configurations.python, {
+      justMyCode = true,
       type = 'python',
       request = 'attach',
-      name = 'attach',
+      name = 'attach_my_code',
+      connect = function()
+        local host = vim.fn.input 'Host [127.0.0.1]: '
+        host = host ~= '' and host or '127.0.0.1'
+        local port = tonumber(vim.fn.input 'Port [5678]: ') or 5678
+        return { host = host, port = port }
+      end,
+    })
+    -- Attach, all code
+    table.insert(dap.configurations.python, {
+      justMyCode = false,
+      type = 'python',
+      request = 'attach',
+      name = 'attach_all_code',
       connect = function()
         local host = vim.fn.input 'Host [127.0.0.1]: '
         host = host ~= '' and host or '127.0.0.1'
