1d2c99a42b1618c982b4c8ad59facc05270b8a79
Andrea Peter <andrea.peter@ecorobotix.com>
2026-07-30 09:48:58 +0200

    treesitter: Install make language

--- init.lua
+++ init.lua
@@ -927,7 +927,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc', 'yaml' }
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'make', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc', 'yaml' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
